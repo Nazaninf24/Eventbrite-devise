f01ee7fadccf0abad3db4520e4b3105fa14432f3
Nazaninf24 <nazaninf24@gmail.com>
2020-02-18 15:34:03 +0100

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'events/show'
+  get 'events/new'
+  get 'events/index'
+  root 'events#index'
+  get 'static_pages/secret'
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
---
+++ db/migrate/20190731122638_nom_de_ta_migration.rb
@@ -0,0 +1,5 @@
+class NomDeTaMigration < ActiveRecord::Migration[5.2]
+  def change
+  	# ici tu mettras les modifications que tu aimerais apporter à ta base de données
+  end
+end
